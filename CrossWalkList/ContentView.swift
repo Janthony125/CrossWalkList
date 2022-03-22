25576bd84be94fc5294f0b2e2133d85e596c8c1d
Paul Inventado <pinventado@fullerton.edu>
2022-03-21 23:29:55 -0700

    Initial commit

--- CrossWalkList/ContentView.swift
+++ CrossWalkList/ContentView.swift
@@ -8,9 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    // TODO: Replace @State with @SceneStorage
+    @State var crosswalkName: String = ""
+    @State var crosswalkAddress: String = ""
+    
+    // TODO: Replace @State with @AppStorage
+    @State var sizeMultiplier = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                HStack {
+                Text("Crosswalk Submission")
+                    .bold()
+                    .font(.largeTitle)
+                }
+                .padding(.bottom, 30)
+                HStack {
+                    Spacer()
+                    NavigationLink(destination: Settings()) {
+                        Text("⚙️ Settings")
+                    }
+                }
+                HStack {
+                    Text("Crosswalk Name")
+                        .bold()
+                    Spacer()
+                }
+                .padding(.bottom, 5)
+                HStack {
+                    TextField("Crosswalk name", text: $crosswalkName)
+                        .modifier(TextEntry())
+                    Spacer()
+                }
+                .padding(.bottom, 20)
+                HStack {
+                    Text("Crosswalk address")
+                        .bold()
+                    Spacer()
+                }
+                .padding(.bottom, 5)
+                TextEditor(text: $crosswalkAddress)
+                    .modifier(TextEntry())
+                    .padding(.bottom, 30)
+                Button(action: {
+                    print("Submitted crosswalk \(crosswalkName) @ \(crosswalkAddress)")
+                }) {
+                    Text("Submit")
+                        .modifier(ButtonDesign())
+                }
+                .padding(.bottom, 30)
+                            
+                HStack {
+                    Spacer()
+                    // TODO: Replace destination to other CrosswalkInfo variations
+                    NavigationLink(destination: CrossWalkInfo()) {
+                        Text("ℹ️ Information about crosswalks")
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
             .padding()
+        }
+        .font(.custom("Arial", size: CGFloat(18 * sizeMultiplier)))
     }
 }
 
